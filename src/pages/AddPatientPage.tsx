1bd4d4c0b68057387d335fbb48951c9655157513
Rahul Dhokare <dhokarer24@gmail.com>
2026-01-15 16:08:17 +0530

    Qa Defect fix. 15/01 .

--- src/pages/AddPatientPage.tsx
+++ src/pages/AddPatientPage.tsx
@@ -1686,7 +1686,7 @@ export default function AddPatientPage({ open, onClose, onSave, doctorId, clinic
                         inputProps={{
                           max: dayjs().format('YYYY-MM-DD')
                         }}
-                        disabled={loading || readOnly}                      
+                        disabled={loading || readOnly}
                       />
                     </Box>
                     <Box sx={{ width: '50% !important' }}>
@@ -2657,26 +2657,28 @@ export default function AddPatientPage({ open, onClose, onSave, doctorId, clinic
                   />
                 </Box>
               </Grid>
-              <Grid item xs={12} md={3}>
-                <Box sx={{ mb: 2, display: 'flex', alignItems: 'center', height: '100%' }}>
-                  <Box>
-                    <Typography variant="subtitle2" sx={{ mb: 1, fontWeight: 'bold' }}>
-                      Add to Today’s Appointments
-                    </Typography>
-                    <FormControlLabel
-                      control={
-                        <Switch
-                          checked={formData.addToTodaysAppointment}
-                          onChange={(e) => handleInputChange('addToTodaysAppointment', e.target.checked as any)}
-                          disabled={loading || readOnly}
-                          color="primary"
-                        />
-                      }
-                      label=""
-                    />
+              {(!patientId && !readOnly) && (
+                <Grid item xs={12} md={3}>
+                  <Box sx={{ mb: 2, display: 'flex', alignItems: 'center', height: '100%' }}>
+                    <Box>
+                      <Typography variant="subtitle2" sx={{ mb: 1, fontWeight: 'bold' }}>
+                        Add to Today’s Appointments
+                      </Typography>
+                      <FormControlLabel
+                        control={
+                          <Switch
+                            checked={formData.addToTodaysAppointment}
+                            onChange={(e) => handleInputChange('addToTodaysAppointment', e.target.checked as any)}
+                            disabled={loading || readOnly}
+                            color="primary"
+                          />
+                        }
+                        label=""
+                      />
+                    </Box>
                   </Box>
-                </Box>
-              </Grid>
+                </Grid>
+              )}
             </Grid>
           </Grid>
         </Grid>
